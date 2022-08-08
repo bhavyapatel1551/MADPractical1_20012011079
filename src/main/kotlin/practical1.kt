bfaa660a34b1a66ea7c73bbb5b005e5415b23362
bhavyapatel1551 <bhavyapatel20@gnu.ac.in>
2022-08-08 17:50:45 +0530

    PRACTICAL1 AND 2

--- src/main/kotlin/practical1.kt
+++ src/main/kotlin/practical1.kt
@@ -1,7 +1,23 @@
 fun main(){
-    var x : Int = 100
-    val y = 200
-    println("x=$x")
-    println("y=$y")
-    
+    var i : Int = 100
+    var f:Float=10.5f
+    var c:Char='B'
+    var s:String="Bhavya"
+    var b:Boolean=true
+    var d:Double=15.15
+    var l:Long=20012011079
+    var sh:Short=-2
+    var byte:Byte=127
+
+    println("Integer=$i")
+    println("Float=$f")
+    println("Char=$c")
+    println("String=$s")
+    println("Boolean=$b")
+    println("Double=$d")
+    println("Long=$l")
+    println("Short=$sh")
+    println("Byte=$byte")
+
+
 }
---
+++ src/main/kotlin/practical2.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var x:Int=10
+    var y:String="10"
+    print("Integer to Double :")
+    println(x.toDouble())
+
+    print("String to Integer : ")
+    println(y.toInt())
+
+    print("String to Double : ")
+    println(y.toDouble())
+
+}
