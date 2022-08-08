f962310a304d7c6b2538b588adb515c11c108fb2
Student <patelsejal0953@gmail.com>
2022-08-08 08:42:16 +0530

    practical1.kt

---
+++ src/main/kotlin/practical3.kt
@@ -0,0 +1,32 @@
+fun main(){
+    print("Enter number")
+    var x = readLine()!!.toInt()
+
+    print("Enter your name ")
+    var n= readLine()!!.toString()
+
+    print("Enter your Branch ")
+    var Branch= readLine()!!.toString()
+
+    print("Enter your Class ")
+    var clas= readLine()!!.toString()
+
+    print("Enter your Batch ")
+    var batch= readLine()!!.toString()
+
+    print("Enter your College name ")
+    var clg= readLine()!!.toString()
+
+    print("Enter your Age ")
+    var age= readLine()!!.toInt()
+
+    println("your number is $x")
+    println("Your name is $n")
+    println("Your name is $Branch")
+    println("Your name is $clas")
+    println("Your name is $batch")
+    println("Your name is $clg")
+    println("Your name is $age")
+
+
+}
